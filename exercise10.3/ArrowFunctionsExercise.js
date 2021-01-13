543ec4ef40d2c6970a595ef3e4c8833e5314b9d3
workmoreandtalkless <guohuaromney@hotmail.com>
2021-01-13 10:36:08 -0500

    arrow function

---
+++ exercise10.3/ArrowFunctionsExercise.js
@@ -0,0 +1,16 @@
+// ES5 Map Callback
+// (arr) => {
+//     return arr.map(val => val * 2);
+// }
+
+// answer
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions
+(numbers) =>{
+    var squares = numbers.map(num=>num **2);
+    var even = squares.filter((square)=> square%2===0);
+    return even;
+}
+
+const squareAndFindEvens = numbers.map(num=>num**2).filter(square => square%2 === 0)
