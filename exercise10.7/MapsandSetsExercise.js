449305821c88bb49732cc20d2f743d09bb6bd479
workmoreandtalkless <guohuaromney@hotmail.com>
2021-01-15 12:05:04 -0500

    exercise 10.7

---
+++ exercise10.7/MapsandSetsExercise.js
@@ -0,0 +1,36 @@
+/** #1
+ * [1,2,3,4]
+ * ans {1,2,3,4}
+ */
+
+ /** #2
+  * ?
+  * referee
+  */
+
+/** #3
+ * { [1,2,3] : false
+ * }
+ * 
+ * 0: {Array(3) => true}
+  1: {Array(3) => false}
+ */
+
+/** hasDuplicate
+ * 
+ */
+function hasDuplicate(arr){
+    const set = new set(arr);
+    return set.size !== arr.size;
+}
+
+/** vowelCount
+ * 
+ */
+
+ function vowelCount(str){
+     let map = new map();
+     for(let s of str){
+         map.add(s,)
+     }
+ }
